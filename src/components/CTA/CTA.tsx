2ffebae786677e7c5ecb53cd9694acfd2f592c5c
kayzo1903 <kaykinangukammari@gmail.com>
2024-01-27 22:59:19 +0300

    lets let's change as it can not accept ' signs

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -4,7 +4,7 @@ const CallToAction = () => {
   return (
     <section className='w-full relative bg-skin bg-ctaimage bg-cover bg-no-repeat min-h-[50vh] text-white'>
          <div className='absolute transform top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-4 flex flex-col items-center'>
-              <h3 className='text-2xl sm:text-3xl text-center leading-relaxed capitalize font-semibold'>Let's Get to work</h3>
+              <h3 className='text-2xl sm:text-3xl text-center leading-relaxed capitalize font-semibold'>Let`&apos;`s Get to work</h3>
               <button className='w-fit px-8 py-2 text-base rounded-xl bg-white text-gray-950'>contacts us</button>
          </div>
     </section>
