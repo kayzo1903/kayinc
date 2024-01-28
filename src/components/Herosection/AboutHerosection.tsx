b33d1c53cf367da01a2d163e5b90b5f29df504bc
kayzo1903 <kaykinangukammari@gmail.com>
2024-01-29 02:22:36 +0300

    navlink props added

--- src/components/Herosection/AboutHerosection.tsx
+++ src/components/Herosection/AboutHerosection.tsx
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar'
 const Herosection = () => {
     return (
         <section className='w-full min-h-screen relative bg-abouthero bg-center bg-cover text-white'>
-            <Navbar />
+            <Navbar navLinks={[]} />
             <div className='w-full text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
                 <h3 className='text-4xl lg:text-5xl font-semibold capitalize leading-relaxed'>Why choose Us</h3>
             </div>
