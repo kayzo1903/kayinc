5ff69c28bc3edf5065b91fb8a6304f52fcc021bc
kayzo1903 <kaykinangukammari@gmail.com>
2024-01-28 06:01:56 +0300

    change screen size to full again

--- src/components/Herosection/Herosection.tsx
+++ src/components/Herosection/Herosection.tsx
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar'
 
 const Herosection = () => {
     return (
-        <section className='w-full min-h-[650px] sm:min-h-screen relative bg-heroimage bg-center bg-cover text-white'>
+        <section className='w-full min-h-screen relative bg-heroimage bg-center bg-cover text-white'>
             <Navbar />
             <div className='w-full text-center px-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
                 <h3 className='text-4xl lg:text-5xl font-semibold capitalize leading-relaxed'>Your Global partner</h3>
