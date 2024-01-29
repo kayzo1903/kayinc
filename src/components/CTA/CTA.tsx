3b85562aa2963ef03cfbd0d2ecd5c3301d902237
kayzo1903 <kaykinangukammari@gmail.com>
2024-01-29 22:11:15 +0300

    Testimonial dark theme changes Cta link

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const CallToAction = () => {
@@ -5,7 +6,7 @@ const CallToAction = () => {
     <section className='w-full relative bg-skin bg-ctaimage bg-contain min-h-[50vh] lg:bg-cover lg:bg-no-repeat lg:min-h-[37.8vh] text-white'>
          <div className='absolute transform top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-4 flex flex-col items-center'>
               <h3 className='text-2xl sm:text-3xl text-center leading-relaxed capitalize font-semibold'>let Get to work</h3>
-              <button className='w-fit px-8 py-2 text-base rounded-xl bg-white text-gray-950'>contacts us</button>
+              <Link href={'/Contacts'} className='w-fit px-8 py-2 text-base rounded-xl bg-white text-gray-950 capitalize'>contacts us</Link>
          </div>
     </section>
   )
